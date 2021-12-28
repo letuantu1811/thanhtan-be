c26502708085a0c6f415ed8759e3398fd627ed0e
son <sonc.nguyen@tx-tech.com>
2021-12-28 23:54:27 +0700

    update thongke :

--- routers/client/thongke.route.js
+++ routers/client/thongke.route.js
@@ -9,12 +9,57 @@ router.get("/", async(req, res) => {
         let endDate = req.query.endDate;
         console.log(startDate + " " + endDate);
         const result = await controller.thongKeDoanhThuTheoNgay(startDate, endDate);
-        console.log(result);
-        response.success(res, "success", result)
+        const banle = await controller.thongKeDoanhThuBanLeTheoNgay(startDate, endDate);
+        let chart = await controller.chartDieuTri(startDate, endDate);
+
+        response.success(res, "success", {dieutri: result[0], banle: banle[0], bieudo: chart});
     } catch (err) {
         console.log(err.message);
         response.error(res, "failed", 500)
     }
 });
+
+router.get("/thongkedieutri", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;
+        let dt = await controller.thongKeDoanhThuTheoNgay(startDate, endDate);
+        let cdv = await controller.thongKeCDVTheoNgay(startDate, endDate);
+        cdv = cdv.map(item => { return { thongke: item.thongke * 1000 } });
+        response.success(res, "success", { thanhtien: dt[0].thanhtien - cdv[0].thongke, soluong:  dt[0]})
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get("/thongkebanle", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;
+        const banle = await controller.thongKeDoanhThuBanLeTheoNgay(startDate, endDate);
+        const chart = await controller.chartBanLe(startDate, endDate);
+        response.success(res, "success", {thongke: banle[0], bieudo: chart})
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get("/thongkecongdichvu", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;
+        console.log(startDate + " " + endDate);
+        let result = await controller.thongKeCDVTheoNgay(startDate, endDate);
+        result = result.map(item => { return { thongke: item.thongke * 1000, soluong: item.soluong } });
+        response.success(res, "success", result[0]);
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+
 
 module.exports = router;
--- src.web/controllers/thongke.controller.js
+++ src.web/controllers/thongke.controller.js
@@ -1,8 +1,12 @@
 const phieudieutri = require('../../database/models/phieudieutri');
+const congdichvu = require('../../database/models/congdichvu');
+const banle = require('../../database/models/banle');
 // const giasuc = require('../../database/models/giasuc');
 const { ENUM } = require('../../utils/index');
-const { Op } = require("sequelize");
+const { Op, QueryTypes } = require("sequelize");
 const sequelize = require("sequelize");
+
+
 
 module.exports = {
     thongKeDoanhThuTheoNgay: async(startDate, endDate) => {
@@ -10,7 +14,7 @@ module.exports = {
             console.log(startDate + " " + endDate);
             return await phieudieutri.findAll({
                 attributes: [
-                    [sequelize.fn('sum', sequelize.col('thanhtien')), 'thanhtien'],
+                    [sequelize.fn('sum', sequelize.col('thanhtien')), 'thanhtien'], [sequelize.fn('count', sequelize.col('thanhtien')), 'soluong']
                 ],
                 where: {
                     ngaytao: {
@@ -18,6 +22,78 @@ module.exports = {
                     }
                 }
             })
+        } catch (error) {
+            return error
+        }
+    },
+
+    thongKeDoanhThuBanLeTheoNgay: async(startDate, endDate) => {
+        try {
+            console.log(startDate + " " + endDate);
+            return await banle.findAll({
+                attributes: [
+                    [sequelize.fn('sum', sequelize.col('tongdonhang')), 'thanhtien'], [sequelize.fn('count', sequelize.col('tongdonhang')), 'soluong']
+                ],
+                where: {
+                    ngaytao: {
+                        [Op.between]: [startDate, endDate]
+                    }
+                }
+            })
+        } catch (error) {
+            return error
+        }
+    },
+
+    thongKeCDVTheoNgay: async(startDate, endDate) => {
+        try {
+            console.log(startDate + " " + endDate);
+            return await phieudieutri.sequelize.query(`select sum(c.gia) as thongke, count(c.gia) as soluong from phieudieutri p2 
+            left join phieudieutri_congdichvu pc4 on pc4.phieudieutri_id  = p2.id 
+            left join congdichvu c on c.id = pc4.congdichvu_id 
+            where p2.ngaytao BETWEEN '${startDate}' and '${endDate}'`, { type: QueryTypes.SELECT });
+        } catch (error) {
+            return error
+        }
+    },
+
+    chartDieuTri: async(startDate, endDate) => {
+        try {
+            console.log(startDate + " " + endDate);
+            return await phieudieutri.sequelize.query(`
+            SELECT
+                YEAR(ngaytao) AS year,
+                MONTH(ngaytao) AS month,
+                SUM(thanhtien) AS thanhtien
+            FROM
+                phieudieutri
+            where
+                ngaytao BETWEEN '${startDate}' and '${endDate}' 
+            GROUP BY
+                YEAR(ngaytao),
+                MONTH(ngaytao)
+            `, { type: QueryTypes.SELECT });
+        } catch (error) {
+            return error
+        }
+    },
+
+    chartBanLe: async(startDate, endDate) => {
+        try {
+            console.log(startDate + " " + endDate);
+            return await phieudieutri.sequelize.query(`
+            SELECT
+                YEAR(ngaytao) AS year,
+                MONTH(ngaytao) AS month,
+                SUM(tongdonhang) AS thanhtien
+            FROM
+                banle
+            where
+                ngaytao BETWEEN '${startDate}' and '${endDate}' 
+            GROUP BY
+                YEAR(ngaytao),
+                MONTH(ngaytao)
+            `, { type: QueryTypes.SELECT });
         } catch (error) {
             return error
         }
