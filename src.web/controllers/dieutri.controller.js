d82e2ef2699db432e885cb759589f6a590fed36a
“Éc <hongquyennthq@gmail.com>
2023-03-30 21:40:27 +0700

    fix: error get exam by petID

--- src.web/controllers/dieutri.controller.js
+++ src.web/controllers/dieutri.controller.js
@@ -491,28 +491,28 @@ module.exports = {
     },
     getAllExamByPetId: async(id, phieudieutriid) => {
         try {
-            let arr = [];
-            let obj = {};
+            // let arr = [];
+            // let obj = {};
             console.log(phieudieutriid);
-            if (
-                phieudieutriid == null ||
-                phieudieutriid == "null" ||
-                phieudieutriid == ""
-            ) {
-                obj = {
-                    giasuc_id: id,
-                };
-            } else {
-                arr = phieudieutriid.split(" ");
-                arr = arr.filter((item) => {
-                    return item !== "undefined";
-                });
-                obj = {
-                    mapping_id: {
-                        [Op.in]: arr,
-                    },
-                };
-            }
+            // if (
+            //     phieudieutriid == null ||
+            //     phieudieutriid == "null" ||
+            //     phieudieutriid == ""
+            // ) {
+            //     obj = {
+            //         giasuc_id: id,
+            //     };
+            // } else {
+            //     arr = phieudieutriid.split(" ");
+            //     arr = arr.filter((item) => {
+            //         return item !== "undefined";
+            //     });
+            //     obj = {
+            //         mapping_id: {
+            //             [Op.in]: arr,
+            //         },
+            //     };
+            // }
 
             return await model.findAll({
                 include: [{
@@ -526,7 +526,7 @@ module.exports = {
                 ],
                 where: {
                     trangthai: 1,
-                    ...obj,
+                    giasuc_id: id,
                 },
                 order: [
                     ["ngaytao", "DESC"]
