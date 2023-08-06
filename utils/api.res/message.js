73720a1257987f704dba6057e6a862f6cecbb2be
Doan Thi Ngoc Lan <lan.doan@didongviet.vn>
2023-08-06 11:34:53 +0700

    update barcode

--- utils/api.res/message.js
+++ utils/api.res/message.js
@@ -2,76 +2,75 @@ const messageJson = require('./message.json');
 const _ = require('lodash');
 
 const getMessage = (key = '') => {
-  // Check empty key.
-  if (_.isEmpty(key)) {
-    return '';
-  }
-  let rootNode = messageJson.res;
-  // get child keys.
-  const keys = key.split('.');
-
-  // Get result message data.
-  let resultMessage = '';
-  _.map(keys, (item) => {
-    const valueOfKey = rootNode[item];
-    if (_.isObject(valueOfKey)) {
-      rootNode = valueOfKey;
-    } else {
-      if (_.isString(valueOfKey)) {
-        resultMessage = valueOfKey;
-      }
-      if (_.isInteger(valueOfKey)) {
-        resultMessage = valueOfKey;
-      }
+    // Check empty key.
+    if (_.isEmpty(key)) {
+        return '';
     }
-  })
-  return resultMessage;
+    let rootNode = messageJson.res;
+    // get child keys.
+    const keys = key.split('.');
+
+    // Get result message data.
+    let resultMessage = '';
+    _.map(keys, (item) => {
+        const valueOfKey = rootNode[item];
+        if (_.isObject(valueOfKey)) {
+            rootNode = valueOfKey;
+        } else {
+            if (_.isString(valueOfKey)) {
+                resultMessage = valueOfKey;
+            }
+            if (_.isInteger(valueOfKey)) {
+                resultMessage = valueOfKey;
+            }
+        }
+    });
+    return resultMessage;
 };
 
 const getMessageValidate = (key = '') => {
-  // Check empty key.
-  if (_.isEmpty(key)) {
-    return '';
-  }
-  let rootNode = messageJson.validate;
-  // get child keys.
-  const keys = key.split('.');
-
-  // Get result message data.
-  let resultMessage = '';
-  _.map(keys, (item) => {
-    let param = item.includes('-')
-    const valueOfKey = rootNode[param ? item.split('-')[0] : item];
-    if (_.isObject(valueOfKey)) {
-      rootNode = valueOfKey;
-    } else {
-      if (_.isString(item.split('-')[0])) {
-        resultMessage = item.split('-')[1] +" "+ valueOfKey;
-      }
+    // Check empty key.
+    if (_.isEmpty(key)) {
+        return '';
     }
-  })
-  return resultMessage;
+    let rootNode = messageJson.validate;
+    // get child keys.
+    const keys = key.split('.');
+
+    // Get result message data.
+    let resultMessage = '';
+    _.map(keys, (item) => {
+        let param = item.includes('-');
+        const valueOfKey = rootNode[param ? item.split('-')[0] : item];
+        if (_.isObject(valueOfKey)) {
+            rootNode = valueOfKey;
+        } else {
+            if (_.isString(item.split('-')[0])) {
+                resultMessage = item.split('-')[1] + ' ' + valueOfKey;
+            }
+        }
+    });
+    return resultMessage;
 };
 
 const success = (message, data) => {
-  return {
-    status: true,
-    message: message,
-    data: data
-  }
-
-}
+    return {
+        status: true,
+        message: message,
+        data: data,
+    };
+};
 const error = (message, code, data) => {
-  return {
-    status: false,
-    code: code,
-    message: message,
-    data: data
-  }
-}
+    return {
+        status: false,
+        code: code,
+        message: message,
+        data: data,
+    };
+};
 module.exports = {
-  getMessage,
-  getMessageValidate,
-  success,
-  error
+    getMessage,
+    getMessageValidate,
+    success,
+    error,
 };
