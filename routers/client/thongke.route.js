8ea787d74b9216dbceddf2bf3f2e6d59cc441666
thuanlequang <thuanle.jr@gmail.com>
2024-03-19 22:09:21 +0700

    update thongke

--- routers/client/thongke.route.js
+++ routers/client/thongke.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../src.web/controllers/thongke.controller");
 const response = require('../../utils/api.res/response');
+const moment = require('moment');
 
 router.get("/", async(req, res) => {
     try {
@@ -107,7 +108,190 @@ router.get("/chartcongdichvu", async(req, res) => {
     }
 });
 
+router.get("/thongkedoanhthu", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;
 
+        if (startDate && endDate) {
+            startDate = moment(startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+            endDate = moment(endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        }
+
+        let dt_dieutri = await controller.thongKeDoanhThuTheoNgay(startDate, endDate);
+        let chart_dieutri = await controller.chartDieuTri(startDate, endDate);
+
+        const dt_banle = await controller.thongKeDoanhThuBanLeTheoNgay(startDate, endDate);
+        const chart_banle = await controller.chartBanLe(startDate, endDate);
+
+        let obj_dieutri = {
+            thang: [],
+            tong_tien_theo_thang: []
+        }
+        for (let i = 0; i < chart_dieutri.length; i++) {
+            obj_dieutri.thang.push("Tháng " + chart_dieutri[i].month);
+            obj_dieutri.tong_tien_theo_thang.push(chart_dieutri[i].thanhtien);
+        }
+
+        let obj_banle = {
+            thang: [],
+            tong_tien_theo_thang: []
+        }
+        for (let i = 0; i < chart_banle.length; i++) {
+            obj_banle.thang.push("Tháng " + chart_banle[i].month);
+            obj_banle.tong_tien_theo_thang.push(chart_banle[i].thanhtien);
+        }
+        response.success(res, "success", 
+        { 
+            doanh_thu: 
+            { 
+                dieutri: dt_dieutri[0],
+                banle: dt_banle[0]
+            }, 
+            bieudo: {
+                dieutri: obj_dieutri,
+                banle: obj_banle
+            } 
+        })
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get("/dieutribypayment", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;     
+        if (startDate && endDate) {
+            startDate = moment(startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+            endDate = moment(endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        }
+        const obj_null = {
+            thanh_thien: '0',
+            so_luong: 0
+        }
+        let result = await controller.chartDieuTriByPayment(startDate, endDate);
+        response.success(res, "success", 
+        {
+            tien_mat: result[0] ? result [0] : obj_null,
+            chuyen_khoan: result[1] ? result [1] : obj_null,
+            cong_no: result[2] ? result [2] : obj_null
+        });
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get("/dieutribytype", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;     
+        if (startDate && endDate) {
+            startDate = moment(startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+            endDate = moment(endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        }
+        const obj_null = {
+            thanh_thien: '0',
+            so_luong: 0
+        }
+        let result = await controller.chartDieuTriByType(startDate, endDate);
+        response.success(res, "success", 
+        {
+            ngoai_tru: result[0] ? result [0] : obj_null,
+            noi_tru: result[1] ? result [1] : obj_null,
+            tai_nha: result[2] ? result [2] : obj_null
+        });
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get("/topproductseller", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;     
+        if (startDate && endDate) {
+            startDate = moment(startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+            endDate = moment(endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        }
+        let result = await controller.topProductSeller(startDate, endDate);
+        response.success(res, "success", result);      
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get("/topserviceseller", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;     
+        if (startDate && endDate) {
+            startDate = moment(startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+            endDate = moment(endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        }
+        let result = await controller.topServiceSeller(startDate, endDate);
+        response.success(res, "success", result);       
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get("/toproductretail", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;     
+        if (startDate && endDate) {
+            startDate = moment(startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+            endDate = moment(endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        }
+        let total = await controller.totalProductRetail(startDate, endDate);
+        let result = await controller.topProductRetail(startDate, endDate);
+        response.success(res, "success", { tongsoluong: total[0].tongsoluong ?? 0, listproduct: result });       
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get("/topstaffretail", async(req, res) => {
+    try {
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;     
+        if (startDate && endDate) {
+            startDate = moment(startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+            endDate = moment(endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+        }
+        let result = await controller.topStaffRetail(startDate, endDate);
+        response.success(res, "success", result);      
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, "failed", 500)
+    }
+});
+
+router.get('/phieudieutri', async (req, res) => {
+    let startDate = req.query.startDate;
+    let endDate = req.query.endDate;
+    let typeDieutrId = parseInt(req.query.typeDieutrId) || 1;     
+    if (startDate && endDate) {
+        startDate = moment(startDate).startOf('day').format('YYYY-MM-DD HH:mm:ss');
+        endDate = moment(endDate).endOf('day').format('YYYY-MM-DD HH:mm:ss');
+    }
+    const pageSize = parseInt(req.query.pageSize) || 1;
+    const pageNum = parseInt(req.query.pageNum) || 1;
+    try {
+        const result = await controller.getPhieuDieuTri(pageSize, pageNum, startDate, endDate, typeDieutrId);
+        response.success_v2(res, 'Lấy dữ liệu thành công', result.data, result.pagination);
+    } catch (err) {
+        console.log(err.message);
+        response.error(res, 'failed', 500);
+    }
+});
 
 
 module.exports = router;
--- src.web/controllers/thongke.controller.js
+++ src.web/controllers/thongke.controller.js
@@ -2,10 +2,12 @@ const phieudieutri = require('../../database/models/phieudieutri');
 const congdichvu = require('../../database/models/congdichvu');
 const phieudieutri_congdichvu = require('../../database/models/phieudieutri_congdichvu');
 const banle = require('../../database/models/banle');
-// const giasuc = require('../../database/models/giasuc');
+const giasuc = require('../../database/models/giasuc');
+const khachhang = require('../../database/models/khachhang');
 const { ENUM } = require('../../utils/index');
 const { Op, QueryTypes } = require('sequelize');
 const sequelize = require('sequelize');
+const { toNumber } = require('lodash');
 
 module.exports = {
     thongKeDoanhThuTheoNgay: async (startDate, endDate) => {
@@ -181,4 +183,223 @@ module.exports = {
             return error;
         }
     },
+
+    chartDieuTriByPayment: async (startDate, endDate) => {
+        try {
+            return await phieudieutri.sequelize.query(
+                `
+            SELECT
+                SUM(thanhtien) AS thanh_tien,
+                COUNT(*) AS so_luong
+            FROM
+                phieudieutri
+            where
+                payment_id IN (1, 2, 3)
+                AND ngaytao BETWEEN '${startDate}' and '${endDate}' 
+            GROUP BY payment_id
+            `,
+                { type: QueryTypes.SELECT },
+            );
+        } catch (error) {
+            return error;
+        }
+    },
+
+    chartDieuTriByType: async (startDate, endDate) => {
+        try {
+            return await phieudieutri.sequelize.query(
+                `
+            SELECT
+                SUM(thanhtien) AS thanh_tien,
+                COUNT(*) AS so_luong
+            FROM
+                phieudieutri
+            where
+                typedieutri_id IN (1, 2, 3)
+                AND ngaytao BETWEEN '${startDate}' and '${endDate}' 
+            GROUP BY typedieutri_id
+            `,
+                { type: QueryTypes.SELECT },
+            );
+        } catch (error) {
+            return error;
+        }
+    },
+
+    topProductSeller: async (startDate, endDate) => {
+        try {
+            return await sanpham.sequelize.query(
+            `
+            SELECT s.id, s.ten, s.tenthaythe, COUNT(ps.sanpham_id) AS soluong
+            FROM
+                sanpham AS s
+            JOIN phieudieutri_sanpham AS ps ON s.id = ps.sanpham_id
+            where
+                ps.ngaytao BETWEEN '${startDate}' and '${endDate}' 
+            GROUP BY s.id, s.ten
+            ORDER BY soluong DESC
+            LIMIT 10;
+            `,
+                { type: QueryTypes.SELECT },
+            );
+        } catch (error) {
+            return error;
+        }
+    },
+
+    topServiceSeller: async (startDate, endDate) => {
+        try {
+            return await congdichvu.sequelize.query(
+            `
+            SELECT c.id, c.ten, COUNT(pc.congdichvu_id) AS soluong
+            FROM
+                congdichvu AS c
+            JOIN phieudieutri_congdichvu AS pc ON c.id = pc.congdichvu_id
+            where
+                pc.ngaytao BETWEEN '${startDate}' and '${endDate}' 
+            GROUP BY c.id, c.ten
+            ORDER BY soluong DESC
+            LIMIT 10;
+            `,
+                { type: QueryTypes.SELECT },
+            );
+        } catch (error) {
+            return error;
+        }
+    },
+
+    totalProductRetail: async (startDate, endDate) => {
+        try {
+            return await banle.sequelize.query(
+            `
+            SELECT SUM(bs.soluong) AS tongsoluong
+            FROM banle_sanpham AS bs
+            WHERE bs.ngaytao BETWEEN '${startDate}' and '${endDate}';
+            `,
+                { type: QueryTypes.SELECT },
+            );
+        } catch (error) {
+            return error;
+        }
+    },
+
+    topProductRetail: async (startDate, endDate) => {
+        try {
+            return await banle.sequelize.query(
+            `
+            SELECT s.id, s.ten, s.tenthaythe,
+                   COUNT(bs.sanpham_id) AS soluongdon,
+                   SUM(bs.soluong) AS soluong,
+                   SUM(bs.soluong * bs.dongiaban - bs.discountAmount) AS tongtien
+            FROM
+                sanpham AS s
+            JOIN banle_sanpham AS bs ON s.id = bs.sanpham_id
+            where
+                bs.ngaytao BETWEEN '${startDate}' and '${endDate}' 
+            GROUP BY s.id, s.ten
+            ORDER BY soluongdon DESC
+            LIMIT 10;
+            `,
+                { type: QueryTypes.SELECT },
+            );
+        } catch (error) {
+            return error;
+        }
+    },
+
+    topStaffRetail: async (startDate, endDate) => {
+        try {
+            return await banle.sequelize.query(
+            `
+            SELECT tv.id, tv.tendaydu,
+                   COUNT(bl.nguoitao_id) AS soluongdon,
+                   SUM(bl.tongdonhang - bl.discountAmount) AS tongtien
+            FROM
+                thanhvien AS tv
+            JOIN banle AS bl ON tv.id = bl.nguoitao_id
+            where
+                bl.ngaytao BETWEEN '${startDate}' and '${endDate}' 
+            GROUP BY tv.id
+            ORDER BY soluongdon DESC
+            LIMIT 10;
+            `,
+                { type: QueryTypes.SELECT },
+            );
+        } catch (error) {
+            return error;
+        }
+    },
+
+    getPhieuDieuTri: async (pageSize, pageNum, startDate, endDate, typeDieutrId) => {
+        const limit = pageSize;
+        const offset = (pageNum - 1) * limit;
+
+        try {
+            const defaultIncludes = [
+                { 
+                    model: giasuc,
+                    as: 'giasuc'
+                },
+                { 
+                    model: khachhang, 
+                    as: 'khachhang'
+                },
+            ];
+
+            const currentDateTreatments = await phieudieutri.findAll({
+                include: [...defaultIncludes],
+                where: {
+                    trangthai: 1,
+                    ngaytao: {
+                        [Op.between]: [startDate, endDate],
+                    },
+                    typedieutri_id: typeDieutrId
+                },
+                order: [['ngaytao', 'DESC']],
+                limit,
+                offset
+            });
+
+            const total = await phieudieutri.count({
+                include: [...defaultIncludes],
+                where: {
+                    trangthai: 1,
+                    ngaytao: {
+                        [Op.between]: [startDate, endDate],
+                    },
+                    typedieutri_id: typeDieutrId
+                }
+            });
+
+            const totalItems = total; 
+            const totalPages = Math.ceil(totalItems / pageSize);
+            const pagination = {
+                totalPages,
+                currentPage: pageNum,
+                pageSize,
+                totalItems,
+            }; 
+
+            const data = currentDateTreatments.map((treetMent) => {
+                const rawTreetMent = treetMent.toJSON();
+                const discountAmount = toNumber(rawTreetMent.discountAmount) || 0;
+                const addedDiscountAmount = toNumber(rawTreetMent.addedDiscountAmount) || 0;
+                const thanhtien = toNumber(rawTreetMent.thanhtien) || 0;
+
+                const orginTotalAmount =
+                    (thanhtien + discountAmount) / (1 - addedDiscountAmount / 100);
+
+                const reCalculateAmountExamForm = {
+                    ...rawTreetMent,
+                    discountAmount: 0,
+                    addedDiscountAmount: 0,
+                    thanhtien: orginTotalAmount,
+                };
+                return reCalculateAmountExamForm;
+            });
+            return { data, pagination };
+        } catch (error) {
+            return error;
+        }
+    }
 };
